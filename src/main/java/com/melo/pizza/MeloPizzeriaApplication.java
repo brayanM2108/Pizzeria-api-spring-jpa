a6cb23afa33c208774f2dc224234937baa9784dd
brayanM2108 <brayan.acevedo28@gmail.com>
2025-06-26 17:15:03 -0500

    initial commit

---
+++ src/main/java/com/melo/pizza/MeloPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.melo.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeloPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeloPizzeriaApplication.class, args);
+	}
+
+}
